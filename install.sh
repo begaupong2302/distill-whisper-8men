6ed0a8c8e917597337d50e0a4a79ef38dab5f751
Nguyễn Khoa Nhật Thành <86068524+begaupong2302@users.noreply.github.com>
2025-11-26 14:59:31 +0700

    Update and rename environment.yml to install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+conda create --name diswhis2 python=3.11 -y
+conda activate diswhis2 -y
+conda install "ffmpeg" -c conda-forge -y
+pip install accelerate==1.11.0 datasets==3.6.0 evaluate==0.4.6 jiwer==4.0.0 soundfile==0.13.1 transformers==4.57.1 einops==0.8.1 librosa==0.11.0
+pip install torch==2.5.1+cu121 torchvision==0.20.1+cu121 torchcodec==0.1.0+cu121 --extra-index-url https://download.pytorch.org/whl/cu121
+pip install packaging ninja
+pip install flash-attn --no-build-isolation
+
+python snap_download.py
+chmod +x script.sh
+./script.sh
