7decbb928e6ece050b050b9360505a523bd67952
Nguyễn Khoa Nhật Thành <86068524+begaupong2302@users.noreply.github.com>
2025-11-28 16:34:18 +0700

    Update snap_download.py

--- snap_download.py
+++ snap_download.py
@@ -1,6 +1,6 @@
 from huggingface_hub import snapshot_download
 import os
-os.environ["HF_HUB_DISABLE_XET"] = "2"
+os.environ["HF_HUB_DISABLE_XET"] = "1"
 
 snapshot_download(
     repo_id="parkpoongpa/data4whipser",
